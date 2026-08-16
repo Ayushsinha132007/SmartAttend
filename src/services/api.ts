bfabad42f2b47e4b9f9ac16e7af4c3d150438e46
Ayush Kumar <ayushkumar53467@gmail.com>
2026-08-16 12:37:27 +0530

    chore: configure axios api client

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,23 +1,11 @@
-import axios from "axios";
+import axios from "axios"
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5001/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5001/api",
   headers: {
     "Content-Type": "application/json",
   },
-});
+  withCredentials: true,
+})
 
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("accessToken");
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-  },
-  (error) => Promise.reject(error),
-);
-
-export default api;
+export default api
